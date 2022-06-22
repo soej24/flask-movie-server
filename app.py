1b806330115e76f671d3b0d79e332fd57f270217
soej24 <soej24@naver.com>
2022-06-22 17:57:57 +0900

    add

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask_jwt_extended import JWTManager
+from flask_restful import Api
+from config import Config
+from resources.movie import MovieListResource, MovieResource
+
+from resources.user import UserLoginResource, UserLogoutResource, UserRegisterResource, jwt_blacklist
+
+app = Flask(__name__)
+
+# 환경변수 셋팅
+app.config.from_object(Config)
+
+# JWT 토큰 라이브러리 만들기
+jwt = JWTManager(app)
+
+# 로그아웃 된 토큰이 들어있는 set을, jwt 에 알려준다.
+@jwt.token_in_blocklist_loader
+def check_if_token_is_revoked(jwt_header, jwt_payload):
+    jti = jwt_payload['jti']
+    return jti in jwt_blacklist
+
+api = Api(app)
+
+# 경로와 리소스(API 코드)를 연결한다.
+api.add_resource(UserRegisterResource, '/users/register')
+api.add_resource(UserLoginResource, '/users/login')
+api.add_resource(UserLogoutResource, '/users/logout')
+api.add_resource(MovieListResource, '/movie')
+api.add_resource(MovieResource, '/movie/<int:movie_id>')
+
+if __name__=="__main__" :
+    app.run()
--- mysql_connection.py
+++ mysql_connection.py
@@ -2,9 +2,12 @@ import mysql.connector
 
 def get_connection() :
     connection = mysql.connector.connect(
-            host = 'yh.cpos3ccatavx.ap-northeast-2.rds.amazonaws.com',
-            database = 'memo_db',
-            user = 'memo_user2',
-            password = 'memo1234')
+            host = 'yhdb.c8nbdutl9vtz.ap-northeast-2.rds.amazonaws.com',
+            database = 'movie_db',
+            user = 'movie_user1',
+            password = '123hello7')
     
     return connection
+
+
+ # 호스트명은 내 AWS의 RDS -> yh_db -> 엔드포인트 yhdb.c8nbdutl9vtz.ap-northeast-2.rds.amazonaws.com
