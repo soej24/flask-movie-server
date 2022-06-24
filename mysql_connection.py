e5ed2d02290405a4f06e9fb0fb900b4e2ee28a30
soej24 <soej24@naver.com>
2022-06-24 17:21:03 +0900

    add

--- mysql_connection.py
+++ mysql_connection.py
@@ -4,10 +4,7 @@ def get_connection() :
     connection = mysql.connector.connect(
             host = 'yhdb.c8nbdutl9vtz.ap-northeast-2.rds.amazonaws.com',
             database = 'movie_db',
-            user = 'movie_user1',
-            password = '123hello7')
+            user = 'soej',
+            password = 'soej1234')
     
     return connection
-
-
- # 호스트명은 내 AWS의 RDS -> yh_db -> 엔드포인트 yhdb.c8nbdutl9vtz.ap-northeast-2.rds.amazonaws.com
